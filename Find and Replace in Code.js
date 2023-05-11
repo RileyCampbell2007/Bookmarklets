7f4c408c367e48c69b06b052a321d8f03541d9ad
RileyCampbell2007 <116246538+RileyCampbell2007@users.noreply.github.com>
2023-05-11 13:59:03 -0600

    Create Find and Replace in Code.js

---
+++ Find and Replace in Code.js
@@ -0,0 +1,7 @@
+var find = prompt('Find:');
+var replace = prompt('Replace:');
+if (find && replace) {
+    var body = document.body.innerHTML;
+    body = body.replace(new RegExp(find, 'g'), replace);
+    document.body.innerHTML = body;
+}
