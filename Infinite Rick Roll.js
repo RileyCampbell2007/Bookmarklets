c4866bbb62c1fa27be6e2bd5cd3f7bd3fd8505e5
RileyCampbell2007 <116246538+RileyCampbell2007@users.noreply.github.com>
2023-05-11 13:52:56 -0600

    Create Infinite Rick Roll.js

---
+++ Infinite Rick Roll.js
@@ -0,0 +1,9 @@
+constlinks = document.getElementsByTagName('a');
+for (let i = 0; i < links.length; i++) {
+    links[i].removeAttribute('href');
+};
+document.addEventListener('click', function() {
+    setTimeout(function() {
+        window.open('//www.youtube.com/watch?v=xvFZjo5PgG0&autoplay=1');
+    }, 0)
+})
