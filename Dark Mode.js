24cfa3c0bb90c39514f69d3c74f300d4a3adb0cb
RileyCampbell2007 <riley.j.campbell.2007@gmail.com>
2023-05-12 12:35:11 -0600

    Modify Formatting and Fix Bugs
    Modified formatting to improve readability and fixed a bug in the random mode where it creating multiple style tags for random.

--- Dark Mode.js
+++ Dark Mode.js
@@ -1,13 +1,45 @@
 if (typeof set == 'undefined') {
     let styles = document.createElement('style');
-    styles.innerHTML += `.darkmode-dark {    color: #EDEDED !important;    background-color: #1E1E1E !important;    border-color: #EDEDED !important;}.darkmode-light {    color: #2F2F2F !important;    background-color: #F5F5F5 !important;    border-color: #2F2F2F !important;}.darkmode-blue {    color: #EEEEFF !important;    background-color: #222244 !important;    border-color: #EEEEFF !important;}        `;
+    styles.innerHTML += `
+.darkmode-dark {
+    color: #EDEDED !important;
+    background-color: #1E1E1E !important;
+    border-color: #EDEDED !important;
+}
+
+.darkmode-light {
+    color: #2F2F2F !important;
+    background-color: #F5F5F5 !important;
+    border-color: #2F2F2F !important;
+}
+
+.darkmode-blue {
+    color: #EEEEFF !important;
+    background-color: #222244 !important;
+    border-color: #EEEEFF !important;
+}`;
     document.head.appendChild(styles);
     let randStyles = document.createElement('style');
     randStyles.id = 'darkmode-random-color-styles';
-    randStyles.innerHTML = `.darkmode-random {    color: #000000 !important;    background-color: #000000 !important;    border-color: #000000 !important;}        `;
+    randStyles.innerHTML = `
+.darkmode-random {
+    color: #000000 !important;
+    background-color: #000000 !important;
+    border-color: #000000 !important;
+}`;
     document.head.appendChild(randStyles);
     const set = "set";
 };
+
+const removeClasses = function() {
+    document.body.querySelectorAll("*").forEach(el => {
+        el.classList.remove('darkmode-random'),
+        el.classList.remove('darkmode-dark'),
+        el.classList.remove('darkmode-light'),
+        el.classList.remove('darkmode-blue')
+    });
+};
+
 let mode = prompt("Enter mode (dark, light, blue, original, random):");
 if (mode == 'random') {
     let hexColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
@@ -19,32 +51,26 @@ if (mode == 'random') {
     document.head.removeChild(document.getElementById('darkmode-random-color-styles'));
     let randStyles = document.createElement('style');
     randStyles.id = 'darkmode-random-color-styles';
-    randStyles.innerHTML = `.darkmode-random {    color: ` + textColor + ` !important;    background-color: ` + hexColor + ` !important;    border-color: ` + textColor + ` !important;}        `;
+    randStyles.innerHTML = `
+.darkmode-random {
+    color: ` + textColor + ` !important;
+    background-color: ` + hexColor + ` !important;
+    border-color: ` + textColor + ` !important;
+}`;
     document.head.appendChild(randStyles);
-    document.querySelectorAll("*").forEach(el => el.classList.add('darkmode-random'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-dark'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-light'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-blue'));
+    removeClasses();
+    document.body.querySelectorAll("*").forEach(el => el.classList.add('darkmode-random'));
 } else if (mode == 'dark') {
-    document.querySelectorAll("*").forEach(el => el.classList.add('darkmode-dark'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-light'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-blue'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-random'));
+    removeClasses();
+    document.body.querySelectorAll("*").forEach(el => el.classList.add('darkmode-dark'));
 } else if (mode == 'light') {
-    document.querySelectorAll("*").forEach(el => el.classList.add('darkmode-light'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-dark'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-blue'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-random'));
+    removeClasses();
+    document.body.querySelectorAll("*").forEach(el => el.classList.add('darkmode-light'));
 } else if (mode == 'blue') {
-    document.querySelectorAll("*").forEach(el => el.classList.add('darkmode-blue'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-dark'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-light'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-random'));
+    removeClasses();
+    document.body.querySelectorAll("*").forEach(el => el.classList.add('darkmode-blue'));
 } else if (mode == 'original') {
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-dark'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-light'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-blue'));
-    document.querySelectorAll("*").forEach(el => el.classList.remove('darkmode-random'));
+    removeClasses();
 } else {
     alert('Invalid mode!');
 }
